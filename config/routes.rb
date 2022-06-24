93aab5b8934735c28b453f9155edec716f5e6837
Oliver Curting <oc@hejverden.dk>
2022-06-24 15:17:21 +0200

    Add luft-til-luft

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   scope controller: :static do
     get :index
     get :calculator
-    get :pricing
+    get :luft_til_vand, path: "luft-til-vand"
+    get :luft_til_luft, path: "luft-til-luft"
   end
 
   root 'static#index'
