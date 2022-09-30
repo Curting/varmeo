cb6b883b60f8bda5751000f2bc64d5e43f70e2de
Oliver Curting <oc@hejverden.dk>
2022-09-30 13:49:57 +0200

    HERO campaign on separate page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     get :index
     get :luft_til_vand, path: "luft-til-vand"
     get :luft_til_luft, path: "luft-til-luft"
-    get :hero_luft_til_luft, path: "hero"
+    get :hero, path: "luft-til-luft/hero"
   end
 
   root 'static#index'
