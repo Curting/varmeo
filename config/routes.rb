b959034d25d5fb32c9e99fc5d003847c913c58ec
Oliver Curting <oc@hejverden.dk>
2022-11-28 20:35:16 +0100

    Add luft til vand form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope controller: :static do
     get :index
     get :luft_til_vand, path: "luft-til-vand"
+    get :luft_til_vand_form, path: "luft-til-vand/form"
     get :luft_til_luft, path: "luft-til-luft"
     get :hero, path: "luft-til-luft/hero"
     get :persondatapolitik
